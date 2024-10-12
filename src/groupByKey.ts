afe5bdf15f800af89e52808dc39cae98a92f60aa
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-10-12 13:42:30 +0300

    add some changes 2

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -10,24 +10,13 @@ type Obj = {
 
 export function groupByKey(items: Obj[], key: keyof Obj): GroupsMap<Obj> {
   const result: GroupsMap<Obj> = {};
-  const moddedItems = items.map((item1) => {
-    const arrayForItems: Obj[] = [];
 
-    items.filter((item2) => {
-      if (item2[key] === item1[key]) {
-        arrayForItems.push(item2);
-
-        return true;
-      }
-
-      return false;
-    });
-
-    return arrayForItems;
-  });
-
-  items.forEach((item, index) => {
-    result[item[key]] = moddedItems[index];
+  items.forEach((item) => {
+    if (!(item[key] in result)) {
+      result[item[key]] = [item];
+    } else {
+      result[item[key]].push(item);
+    }
   });
 
   return result;
